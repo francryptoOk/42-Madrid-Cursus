c7d44eb957b545af8ab77e71d70a4c552d5a5e60
Francisco Guido Santillan <fsantill@c1r6s6.42madrid.com>
2024-02-14 15:27:17 +0100

    an opening window

--- Solong/so_long.c
+++ Solong/so_long.c
@@ -6,7 +6,7 @@
 /*   By: fsantill <fsantill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 16:03:30 by fsantill          #+#    #+#             */
-/*   Updated: 2024/02/14 14:51:26 by fsantill         ###   ########.fr       */
+/*   Updated: 2024/02/14 15:24:56 by fsantill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,16 @@
 
 char	**parsing_map_and_add(int argc, char **argv, t_map **map)
 {
-	int		file;
-	char	**map_aux = NULL;
+	int		fd;
+	char	**map_aux;
 	int		y;
 	int		x;
 
+	map_aux = NULL;
 	if (argc > 1)
 	{
-		file = open(argv[1], O_RDONLY);
-		if (!file)
+		fd = open(argv[1], O_RDONLY);
+		if (!fd)
 			return (perror("Error opening file\n"), NULL);
 		map_aux[1] = "";
 		y = 0;
@@ -36,7 +37,7 @@ char	**parsing_map_and_add(int argc, char **argv, t_map **map)
 		{
 			while (map_aux[y][x])
 			{
-				map_aux[y][x] = *get_next_line(file);
+				map_aux[y][x] = *get_next_line(fd);
 				(*map)->map = map_aux;
 				if (map_aux)
 					printf("Map Line:\n%s\n", map_aux[y]);
@@ -49,17 +50,31 @@ char	**parsing_map_and_add(int argc, char **argv, t_map **map)
 	return (printf("\n"), map_aux);
 }
 
-int	main(int argc, char **argv)
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
-	void	*mlx_win;
-	void	*mlx;
-	t_map	*startmap;
+	char	*dst;
 
-	mlx_win = NULL;
-	mlx = NULL;
-	startmap->map = parsing_map_and_add(argc, argv, &startmap);
-	mlx_init();
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+int	main(/*int argc, char **argv*/)
+{
+	t_data	img;
+	void	*mlx;
+	void	*mlx_win;
+//	t_map	*startmap;
+
+//	mlx_win = NULL;
+//	mlx = NULL;
+//	startmap->map = parsing_map_and_add(argc, argv, &startmap);
+	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Game");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, \
+	&img.line_length, &img.endian);
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 	return (0);
 }
--- Solong/so_long.h
+++ Solong/so_long.h
@@ -6,7 +6,7 @@
 /*   By: fsantill <fsantill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 16:05:15 by fsantill          #+#    #+#             */
-/*   Updated: 2024/02/14 14:51:25 by fsantill         ###   ########.fr       */
+/*   Updated: 2024/02/14 15:20:05 by fsantill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,15 @@
 # define SO_LONG_H
 # include "libft/libft.h"
 # include "minilibx_opengl_20191021/mlx.h"
+
+typedef struct t_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}					t_data;
 
 typedef struct t_map
 {
