57cfd8eff430593dc718e42ada764bbe2342f9b0
Francisco Guido Santillan <fsantill@c1r9s4.42madrid.com>
2024-06-27 12:14:28 +0200

    I added some comments

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -6,7 +6,7 @@
 /*   By: fsantill <fsantill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 16:03:30 by fsantill          #+#    #+#             */
-/*   Updated: 2024/06/20 16:42:36 by fsantill         ###   ########.fr       */
+/*   Updated: 2024/06/27 12:13:59 by fsantill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,30 +28,30 @@ void	ft_son(char **argv, t_pip father, char **env)
 {
 	int	fd;
 
-	close(father.fd[0]); //I don't use this, so it's better closing
-	fd = open(argv[1], O_RDONLY);
+	close(father.fd[0]); //I don't use this reading FD in child process, so it's better closing
+	fd = open(argv[1], O_RDONLY); //open in read only
 	if (fd == -1)
 		ft_exit_msg("Error\n\t• Problem open infile", 1);
 	dup2(fd, STDIN_FILENO); //Here I change the input from standard (terminal) to FD (infile)
 	dup2(father.fd[1], STDOUT_FILENO); //Here I change the output from standard to FD[1] (in structure)
-	close(fd); //I ended using the FD (open infile), so it's better closing
-	execve(argv[2], &argv[2], env);
-	ft_exit_msg("execve", 1);
+	close(fd); //I ended using the FD (the open infile), so it's better closing
+	execve(argv[2], &argv[2], env); //First command execution
+	ft_exit_msg("execve", 1); //If execve fails, exit with perror
 }
 
 void	ft_father(char **argv, t_pip father, char **env)
 {
 	int	fd;
 
-	fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+	close(father.fd[1]); //I don't use this writing FD in parent process, so it's better closing
+	fd = open(argv[4], O_WRONLY | O_TRUNC | O_CREAT, 0666); // open in write only, trunc file (erasing old data in it) or create new file (if doesn't exist), 0666 for all permitions
 	if (fd == -1)
 		ft_exit_msg("Error\n\t• Problem open outfile", 1);
-	dup2(fd, 1);
-	dup2(father.fd[0], 0);
-	close(father.fd[1]);
-	close(fd);
-	execve(argv[3], &argv[3], env);
-	ft_exit_msg("execve", 1);
+	dup2(fd, STDOUT_FILENO); //Here I change the output from standard (terminal) to FD (infile)
+	dup2(father.fd[0], STDIN_FILENO); //Here I change the output from standard to FD[0] (in structure)
+	close(fd); //I ended using the FD (the open outfile), so it's better closing
+	execve(argv[3], &argv[3], env); //Second command execution
+	ft_exit_msg("execve", 1); //If execve fails, exit with perror
 }
 
 int	main(int argc, char **argv, char **env)
