40ec32b87c88616ed23a178dcc701f989b8cff24
Francisco Guido Santillan <fsantill@c1r9s5.42madrid.com>
2024-07-02 13:03:34 +0200

    pipex functional

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -6,7 +6,7 @@
 /*   By: fsantill <fsantill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 16:03:30 by fsantill          #+#    #+#             */
-/*   Updated: 2024/07/01 12:28:31 by fsantill         ###   ########.fr       */
+/*   Updated: 2024/07/02 13:02:31 by fsantill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,14 @@ char	**ft_path(char **env)
 	while (env[i])
 	{
 		if (ft_strnstr(env[i], "PATH=", ft_strlen("PATH=")))
+		{
 			route = ft_split(env[i] + ft_strlen("PATH="), ':');
+			return (route);
+		}
 		i++;
 	}
-	if (!route)
-		ft_exit_msg("Error\n\t• Path/Route not found", 1);
-	return(route);
+	ft_exit_msg("Error\n\t• Path/Route not found", 1);
+	return (NULL);
 }
 
 //	i = -1;
@@ -40,23 +42,26 @@ char	**ft_path(char **env)
 
 char	*ft_valid_cmd_path(char **route, char *argv)
 {
-	char *cmd_path;
+	char *path_with_bar;
+	char *path_with_cmd;
 	int	i;
 
 	i = 0;
 	while (route[i])
 	{
-		cmd_path = ft_strjoin(route[i], argv);
-		if (access(cmd_path, X_OK))
-			return(cmd_path);
-		free(cmd_path);
+		path_with_bar = ft_strjoin(route[i], "/");
+		path_with_cmd = ft_strjoin(path_with_bar, argv);
+		free(path_with_bar);
+		if (access(path_with_cmd, X_OK) == 0)
+			return (path_with_cmd);
+		free(path_with_cmd);
 		i++;
 	}
 	ft_exit_msg("Error\n\t• Invalid cmd on Path/Route", 1);
 	return (NULL);
 }
 
-void	ft_son(char **argv, t_pip father, char **env)
+void	ft_son_one(char **argv, t_pip father, char **env)
 {
 	int	fd;
 	char	**route;
@@ -72,12 +77,15 @@ void	ft_son(char **argv, t_pip father, char **env)
 	close(fd); //I ended using the FD (the open infile), so it's better closing
 	route = ft_path(env);
 	cmdflagsplit = ft_split(argv[2], ' ');
-	validcmd = ft_valid_cmd_path(route, argv[2]);
+	validcmd = ft_valid_cmd_path(route, cmdflagsplit[0]);
 	execve(validcmd, cmdflagsplit, env); //First command execution
+	ft_arr_free(route);
+	ft_arr_free(cmdflagsplit);
+	free(validcmd);
 	ft_exit_msg("Error\n\t• execve failed", 1);
 }
 
-void	ft_father(char **argv, t_pip father, char **env)
+void	ft_son_two(char **argv, t_pip father, char **env)
 {
 	int		fd;
 	char	**route;
@@ -88,13 +96,16 @@ void	ft_father(char **argv, t_pip father, char **env)
 	fd = open(argv[4], O_WRONLY | O_TRUNC | O_CREAT, 0666); // open outfile in write only, trunc file (erasing old data in it) or create new file (if doesn't exist), 0666 for all permitions
 	if (fd == -1)
 		ft_exit_msg("Error\n\t• Problem opening outfile", 1);
-	dup2(fd, STDOUT_FILENO); //Here I change the output from standard (terminal) to FD (infile)
-	dup2(father.fd[0], STDIN_FILENO); //Here I change the output from standard to FD[0] (in structure)
+	dup2(fd, STDOUT_FILENO); //Here I change the output from standard (terminal) to FD (outfile)
+	dup2(father.fd[0], STDIN_FILENO); //Here I change the input from standard to FD[0] (in structure)
 	close(fd); //I ended using the FD (the open outfile), so it's better closing
 	route = ft_path(env);
 	cmdflagsplit = ft_split(argv[3], ' ');
-	validcmd = ft_valid_cmd_path(route, argv[3]);
+	validcmd = ft_valid_cmd_path(route, cmdflagsplit[0]);
 	execve(validcmd, cmdflagsplit, env); //Second command execution
+	ft_arr_free(route);
+	ft_arr_free(cmdflagsplit);
+	free(validcmd);
 	ft_exit_msg("Error\n\t• execve failed", 1);
 }
 
@@ -103,22 +114,22 @@ int	main(int argc, char **argv, char **env)
 	t_pip	father;
 	pid_t	pid;
 
-//	ft_path(env);
 	if (argc == 5)
 	{
 		if (pipe(father.fd) == -1)
 			ft_exit_msg("Error\n\t• Pipe failed", 1);
-		return (0);
 		pid = fork();
 		if (pid == -1)
-			ft_exit_msg("Error\n\t• Fork cannot duplicate process", 1);
+			ft_exit_msg("Error\n\t• Fork cannot duplicate process, 1st try", 1);
 		else if (pid == 0)
-			ft_son(argv, father, env);
-		else
-		{
-			wait(NULL);
-			ft_father(argv, father, env);
-		}
+			ft_son_one(argv, father, env);
+		pid = fork();
+		if (pid == -1)
+			ft_exit_msg("Error\n\t• Fork cannot duplicate process, 2nd try", 1);
+		else if (pid == 0)
+			ft_son_two(argv, father, env);
+		wait(NULL);
+		wait(NULL);
 		return (0);
 	}
 	else
--- pipex/pipex.h
+++ pipex/pipex.h
@@ -6,7 +6,7 @@
 /*   By: fsantill <fsantill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 16:05:15 by fsantill          #+#    #+#             */
-/*   Updated: 2024/07/01 12:12:54 by fsantill         ###   ########.fr       */
+/*   Updated: 2024/07/02 12:56:20 by fsantill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ void	ft_exit_msg(char *msg, int perrorbool);
 void	*ft_arr_free(char **arr);
 char	**ft_path(char **env);
 char	*ft_valid_cmd_path(char **route, char *argv);
-void	ft_son(char **argv, t_pip father, char **env);
-void	ft_father(char **argv, t_pip father, char **env);
+void	ft_son_one(char **argv, t_pip father, char **env);
+void	ft_son_two(char **argv, t_pip father, char **env);
 
 #endif
