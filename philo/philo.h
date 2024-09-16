a249f57d2cc535bc996f76bfe79f02e52b693ec6
Francisco Guido Santillan <fsantill@c1r7s3.42madrid.com>
2024-09-16 13:28:02 +0200

    Starting Philosophers

--- philo/philo.h
+++ philo/philo.h
@@ -6,18 +6,48 @@
 /*   By: fsantill <fsantill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 16:05:15 by fsantill          #+#    #+#             */
-/*   Updated: 2024/09/16 11:50:32 by fsantill         ###   ########.fr       */
+/*   Updated: 2024/09/16 13:26:47 by fsantill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
-# include "libft/libft.h"
 
-//typedef struct s_pip
-//{
-//	int	fd[2];
-//}					t_pip;
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <limits.h>
+# include <stdarg.h>
+# include <sys/wait.h>
+# include <pthread.h>
+
+typedef struct s_philosopher
+{
+	int				id;
+	pthread_t		thread;
+	int				left_fork;
+	int				right_fork;
+	long			last_meal_time;
+	int				times_eaten;
+	pthread_mutex_t	*forks;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+}					t_philosopher;
+
+typedef struct s_data
+{
+	int				number_of_philosophers;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	pthread_mutex_t	*forks;
+	t_philosopher		*philosophers;
+}					t_data;
 
 void	ft_leaks(void);
 void	ft_exit_msg(char *msg, int errorvalue);
